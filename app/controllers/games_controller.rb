b01d0d802b142f6bcf9ca460fa54f48a6cdc7ba7
RaykPlatzek <rayk.platzek@gmail.com>
2017-05-29 22:42:05 +0000

    added the board_design method back as it was deleted

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -10,6 +10,17 @@ class GamesController < ApplicationController
   private
 
   def draw_board
-
+    @board = Array.new(8) { Array.new(8) }
+    column = [1, 2, 3, 4, 5, 6, 7, 8]
+    row = [1, 2, 3, 4, 5, 6, 7, 8]
+    row.each_with_index do |_row, ri|
+      column.each_with_index do |_column, ci|
+        @board[ri][ci] = if ci.even? && ri.even? || ri % 2 > 0 && ci % 2 > 0
+                           'white_field.png'
+                         else
+                           'black_field.png'
+                         end
+      end
+    end
   end
 end
