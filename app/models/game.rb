be290c9caf1e916d4f40bebd221c5b684a71a9f6
Kristin Heininger <dauphindouze@comcast.net>
2017-05-29 23:03:08 +0000

    added tests

--- app/models/game.rb
+++ app/models/game.rb
@@ -11,7 +11,6 @@ class Game < ApplicationRecord
 
     (0..7).each do |i|
       Pawn.create(
-        game_id: id,
         row_coordinate: 6,
         column_coordinate: i,
         color: "white",
@@ -19,125 +18,109 @@ class Game < ApplicationRecord
     end
 
     Rook.create(
-      game_id: id,
       row_coordinate: 7,
       column_coordinate: 0,
-      color: "white";
+      color: "white"
       )
 
     Rook.create(
-      game_id: id,
       row_coordinate: 7,
       column_coordinate: 7,
-      color: "white";
+      color: "white"
       )
 
     Knight.create(
-      game_id: id,
       row_coordinate: 7,
       column_coordinate: 1,
-      color: "white";
+      color: "white"
       )
 
     Knight.create(
-      game_id: id,
       row_coordinate: 7,
       column_coordinate: 6,
-      color: "white";
+      color: "white"
       )
 
     Bishop.create(
-      game_id: id,
       row_coordinate: 7,
       column_coordinate: 2,
-      color: "white";
+      color: "white"
       )
 
     Bishop.create(
-      game_id: id,
       row_coordinate: 7,
       column_coordinate: 5,
-      color: "white";
+      color: "white"
       )
 
     Queen.create(
-      game_id: id,
       row_coordinate: 0,
       column_coordinate: 3,
-      color: "white";
+      color: "white"
       )
 
     King.create(
-      game_id: id,
       row_coordinate: 7,
       column_coordinate: 4,
-      color: "white";
+      color: "white"
       )
 
     # Build black pieces
     (0..7).each do |i|
       Pawn.create(
-        game_id: id,
         row_coordinate: 1,
         column_coordinate: i,
-        color: "black";
+        color: "black"
         )
+    end
 
       Rook.create(
-        game_id: id,
         row_coordinate: 0,
         column_coordinate: 0,
-        color: "black";
+        color: "black"
         )
 
       Rook.create(
-        game_id: id,
         row_coordinate: 0,
         column_coordinate: 7,
-        color: "black";
+        color: "black"
         )
 
       Knight.create(
-        game_id: id,
         row_coordinate: 0,
         column_coordinate: 1,
-        color: "black";
+        color: "black"
         )
 
       Knight.create(
-        game_id: id,
         row_coordinate: 0,
         column_coordinate: 6,
-        color: "black";
+        color: "black"
         )
 
       Bishop.create(
-        game_id: id,
         row_coordinate: 0,
         column_coordinate: 2,
-        color: "black";
+        color: "black"
         )
 
       Bishop.create(
-        game_id: id,
         row_coordinate: 0,
         column_coordinate: 5,
-        color: "black";
+        color: "black"
         )
 
       Queen.create(
-        game_id: id,
         row_coordinate: 0,
         column_coordinate: 3,
-        color: "black";
+        color: "black"
         )
 
       King.create(
-        game_id: id,
         row_coordinate: 0,
         column_coordinate: 4,
-        color: "black";
+        color: "black"
         )
 
-    end
   end
+end
