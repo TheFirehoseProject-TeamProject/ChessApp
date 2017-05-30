6144d83f4160e7fb9152f771cb6cf113314593ca
RaykPlatzek <rayk.platzek@gmail.com>
2017-05-30 05:40:53 +0000

    code refactoring

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -11,10 +11,10 @@ class Piece < ApplicationRecord
     counter = 1
     result = false
     if destination_x > self.column_coordinate
-      (destination_x - self.column_coordinate - 1).times do
-        Piece.where(column_coordinate: destination_x - counter, row_coordinate: self.row_coordinate).count.zero? ? result = false : result = true
+      # (destination_x - self.column_coordinate - 1).times do
+      ((self.column_coordinate + 1)..destination_x-1).each do |col|
+      result = Piece.where(column_coordinate: col, row_coordinate: self.row_coordinate).present?
         return result if result == true
-        counter += 1
       end
     end
     if destination_x < self.column_coordinate
@@ -41,21 +41,24 @@ class Piece < ApplicationRecord
     end
     return result
   end
+# 
+#     array = []
+#     if destination_x > self.column_coordinate
+#       if Piece.where('column_coordinate > ?', self.column_coordinate).present?
+# #         array << Piece.where('column_coordinate > ? and row_coordinate > ?', self.column_coordinate, self.row_coordinate)
+#          array << Piece.where('column_coordinate > ?', self.column_coordinate).where('row_coordinate > ?', self.row_coordinate)
+#          byebug
+#          []
+#          [ [piece1, piece2], [piece3, piece4]].flatten => [piece1, piece2, piece3, piece4]
+#          array.each do |element|
+#            if element.column_coordinate < destination_x && element.column_coordinate != self.column_coordinate
+#              return true
+#            end
+#          end
+#         return false
+#       end
+#     end
 
-  #   array = []
-  #   if destination_x > self.column_coordinate
-  #     if Piece.where('column_coordinate > ?', self.column_coordinate).present?
-  #        array << Piece.where('column_coordinate > ?', self.column_coordinate)
-  #        byebug
-  #        array.each do |element|
-  #          if element.column_coordinate < destination_x && element.column_coordinate != self.column_coordinate
-  #            return true
-  #          end
-  #        end
-  #       return false
-  #     end
-  #   end
-  #
   #   if destination_x < self.column_coordinate
   #     if Piece.where('column_coordinate < ?', self.column_coordinate)
   #       return true
