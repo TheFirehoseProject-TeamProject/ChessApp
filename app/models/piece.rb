98d0f2e84ec7824490081338d02f8caf39f7411c
RaykPlatzek <rayk.platzek@gmail.com>
2017-05-30 04:48:11 +0000

    1. horizontal check in method obstructed created plus TDD 2. vertically check in  method obstructed created pluss TDD

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -7,34 +7,60 @@ class Piece < ApplicationRecord
   end
 
   def obstructed?(destination_x, destination_y)
-    # horizontal
-    # counter = 1
-    # result = false
-    # (destination_x - self.column_coordinate - 1).times do
-    #   Piece.where(column_coordinate: destination_x - counter, row_coordinate: self.row_coordinate).count.zero? ? result = false : result = true
-    #   return result if result == true
-    #   counter += 1
-    # end
-    # return result
-    array = []
+    # ----------------------case 1: horizontal move--------------------------
+    counter = 1
+    result = false
     if destination_x > self.column_coordinate
-      if Piece.where('column_coordinate > ?', self.column_coordinate).present?
-         array << Piece.where('column_coordinate > ?', self.column_coordinate)
-         byebug
-         array.each do |element|
-           if element.column_coordinate < destination_x && element.column_coordinate != self.column_coordinate
-             return true
-           end
-         end
-        return false
+      (destination_x - self.column_coordinate - 1).times do
+        Piece.where(column_coordinate: destination_x - counter, row_coordinate: self.row_coordinate).count.zero? ? result = false : result = true
+        return result if result == true
+        counter += 1
       end
     end
-
     if destination_x < self.column_coordinate
-      if Piece.where('column_coordinate < ?', self.column_coordinate)
-        return true
+      (self.column_coordinate - destination_x - 1).times do
+        Piece.where(column_coordinate: destination_x + counter, row_coordinate: self.row_coordinate).count.zero? ? result = false : result = true
+        return result if result == true
+        counter += 1
       end
     end
-    return false
+    # -----------------------case 2: vertical move---------------------------
+    if destination_y > self.row_coordinate
+      (destination_y - self.row_coordinate - 1).times do
+        Piece.where(row_coordinate: destination_y - counter, column_coordinate: self.column_coordinate).count.zero? ? result = false : result = true
+        return result if result == true
+        counter += 1
+      end
+    end
+    if destination_y < self.row_coordinate
+      (self.row_coordinate - destination_y - 1).times do
+        Piece.where(row_coordinate: destination_y + counter, column_coordinate: self.column_coordinate).count.zero? ? result = false : result = true
+        return result if result == true
+        counter += 1
+      end
+    end
+    return result
   end
+
+  #   array = []
+  #   if destination_x > self.column_coordinate
+  #     if Piece.where('column_coordinate > ?', self.column_coordinate).present?
+  #        array << Piece.where('column_coordinate > ?', self.column_coordinate)
+  #        byebug
+  #        array.each do |element|
+  #          if element.column_coordinate < destination_x && element.column_coordinate != self.column_coordinate
+  #            return true
+  #          end
+  #        end
+  #       return false
+  #     end
+  #   end
+  #
+  #   if destination_x < self.column_coordinate
+  #     if Piece.where('column_coordinate < ?', self.column_coordinate)
+  #       return true
+  #     end
+  #   end
+  #   return false
+  # end
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -1,15 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Piece, type: :model do
-  describe '.is_obstructed horizontally' do
+
+  describe 'method obstruced # checking if fields are obstructed horizontally' do
+    it 'should return false if no piece exists between destination and origin (left or right)' do
+      game1 = Game.create(id: 1)
+      user1 = User.create(id: 1, email: '123@123.com', password: '123456', game_id: 1)
+      piece1 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 4, row_coordinate: 0)
+      expect(piece1.obstructed?(0, 0)).to eq false #checking left side
+      expect(piece1.obstructed?(7, 0)).to eq false #checking right side
+    end
+    it "should return true if piece is between destination and origin (left or right) " do
+      game1 = Game.create(id: 1)
+      user1 = User.create(id: 1, email: '123@123.com', password: '123456', game_id: 1)
+      piece1 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 4, row_coordinate: 0)
+      piece2 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 1, row_coordinate: 0)
+      piece3 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 6, row_coordinate: 0)
+      expect(piece1.obstructed?(0, 0)).to eq true #checking left side
+      expect(piece1.obstructed?(7, 0)).to eq true  #checking right side
+    end
+    it "should return false if a piece is at the destination field (left or right)" do
+      game1 = Game.create(id: 1)
+      user1 = User.create(id: 1, email: '123@123.com', password: '123456', game_id: 1)
+      piece1 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 4, row_coordinate: 0)
+      piece2 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 0, row_coordinate: 0)
+      piece3 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 7, row_coordinate: 0)
+      expect(piece1.obstructed?(0, 0)).to eq false  #checking left side
+      expect(piece1.obstructed?(7, 0)).to eq false  #checking right side
+    end
+  end
+
+  describe 'method obstruced # checking if fields are obstructed vertically' do
     it 'should return false if no piece exists between' do
       game1 = Game.create(id: 1)
       user1 = User.create(id: 1, email: '123@123.com', password: '123456', game_id: 1)
-      piece1 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 0, row_coordinate: 0)
-      piece2 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 5, row_coordinate: 0)
-      piece2
-      expect(piece1.obstructed?(3, 0)).to eq false
-      expect(piece1.obstructed?(6, 0)).to eq true
+      piece1 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 4, row_coordinate: 4)
+      expect(piece1.obstructed?(4, 0)).to eq false
+    end
+    it "should return true if piece is between destination and origin (below or above) " do
+      game1 = Game.create(id: 1)
+      user1 = User.create(id: 1, email: '123@123.com', password: '123456', game_id: 1)
+      piece1 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 4, row_coordinate: 4)
+      piece2 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 4, row_coordinate: 2)
+      piece3 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 4, row_coordinate: 6)
+      expect(piece1.obstructed?(4, 0)).to eq true  #checking below
+      expect(piece1.obstructed?(4, 7)).to eq true  #checking above
+    end
+    it "should return false if a piece is at the destination field (ablow or above)" do
+      game1 = Game.create(id: 1)
+      user1 = User.create(id: 1, email: '123@123.com', password: '123456', game_id: 1)
+      piece1 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 4, row_coordinate: 4)
+      piece2 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 4, row_coordinate: 0)
+      piece3 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 4, row_coordinate: 7)
+      expect(piece1.obstructed?(4, 0)).to eq false #checking below
+      expect(piece1.obstructed?(4, 7)).to eq false #checking above
     end
   end
 end
