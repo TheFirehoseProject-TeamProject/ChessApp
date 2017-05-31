9e8ec74d469ee42e19ed2c1756582a30264bfef3
RaykPlatzek <rayk.platzek@gmail.com>
2017-05-31 04:10:00 +0000

    unfinished version

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -7,33 +7,76 @@ class Piece < ApplicationRecord
   end
 
   def obstructed?(destination_x, destination_y)
-    # ----------------------case 1: horizontal move--------------------------
-    result = false
-    if destination_x > self.column_coordinate
-      ((self.column_coordinate + 1)..(destination_x-1)).each do |col|
-      result = Piece.where(column_coordinate: col, row_coordinate: self.row_coordinate).present?
-        return result if result == true
-      end
-    end
-    if destination_x < self.column_coordinate
-      ((destination_x+1)..(self.column_coordinate - 1)).each do |col|
+      result = false
+      length1_perp = (destination_x - destination_y)
+      length2_perp = (self.column_coordinate - self.row_coordinate)
+      length1_diag = (self.column_coordinate - destination_x)
+      length2_diag = (destination_y - self.row_coordinate)
+
+      return "Error Message" if destination_y > 7 || destination_x > 7 || destination_y < 0 || destination_x < 0
+
+      # ----------------------case 1: horizontal move--------------------------
+      if destination_x > self.column_coordinate && destination_y == self.row_coordinate
+        ((self.column_coordinate + 1)..(destination_x-1)).each do |col|
         result = Piece.where(column_coordinate: col, row_coordinate: self.row_coordinate).present?
-        return result if result == true
+          return result if result == true
+        end
       end
-    end
-    # -----------------------case 2: vertical move---------------------------
-    if destination_y > self.row_coordinate
-      ((self.row_coordinate + 1)..(destination_y -1)).each do |row|
-        result = Piece.where(row_coordinate: row, column_coordinate: self.column_coordinate).present?
-        return result if result == true
+      if destination_x < self.column_coordinate && destination_y == self.row_coordinate
+        ((destination_x+1)..(self.column_coordinate - 1)).each do |col|
+          result = Piece.where(column_coordinate: col, row_coordinate: self.row_coordinate).present?
+          return result if result == true
+        end
       end
-    end
-    if destination_y < self.row_coordinate
-      ((destination_y + 1)..(self.row_coordinate - 1)).each do |row|
-        result = Piece.where(row_coordinate: row, column_coordinate: self.column_coordinate).present?
-        return result if result == true
+      # -----------------------case 2: vertical move---------------------------
+      if destination_y > self.row_coordinate && destination_x == self.column_coordinate
+        ((self.row_coordinate + 1)..(destination_y -1)).each do |row|
+          result = Piece.where(row_coordinate: row, column_coordinate: self.column_coordinate).present?
+          return result if result == true
+        end
       end
-    end
+      if destination_y < self.row_coordinate && destination_x == self.column_coordinate
+        ((destination_y + 1)..(self.row_coordinate - 1)).each do |row|
+          result = Piece.where(row_coordinate: row, column_coordinate: self.column_coordinate).present?
+          return result if result == true
+        end
+      end
+      #-----------------------case 3: diagonal move up right-------------------------
+      if destination_y > self.row_coordinate && destination_x > self.column_coordinate && length1_perp == length2_perp
+        ((self.row_coordinate + 1 )..(destination_y - 1)).each_with_index do |row, index|
+          index+= 1
+          result = Piece.where(row_coordinate: row, column_coordinate: self.column_coordinate + index).present?
+          return result if result == true
+        end
+      end
+      #-----------------------case 4: diagonal move down left-------------------------
+      if destination_y < self.row_coordinate && destination_x < self.column_coordinate && length1_perp == length2_perp
+        ((destination_y + 1)..(self.row_coordinate - 1 )).each_with_index do |row, index|
+          index+= 1
+          result = Piece.where(row_coordinate: row, column_coordinate: self.column_coordinate - index).present?
+          return result if result == true
+        end
+      end
+
+
+      #-----------------------case 5: diagonal move up left-------------------------
+            if destination_y > self.row_coordinate && destination_x < self.column_coordinate && length1_diag == length2_diag
+        ((self.row_coordinate + 1 )..(destination_y - 1)).each_with_index do |row, index|
+          index+= 1
+          result = Piece.where(row_coordinate: row, column_coordinate: self.column_coordinate - index).present?
+          return result if result == true
+        end
+      end
+      #-----------------------case 6: diagonal move down right-------------------------
+      if destination_y < self.row_coordinate && destination_x > self.column_coordinate
+        ((destination_y + 1)..(self.row_coordinate - 1 )).to_a.reverse.each_with_index do |row, index|
+          index+= 1
+          result = Piece.where(row_coordinate: row, column_coordinate: self.column_coordinate + index).present?
+          return result if result == true
+        end
+      end
+
+
     return result
   end
 end
--- spec/models/piece_spec.rb
+++ spec/models/piece_spec.rb
@@ -4,7 +4,9 @@ RSpec.describe Piece, type: :model do
   before do
       game1 = Game.create(id: 1)
       user1 = User.create(id: 1, email: '123@123.com', password: '123456', game_id: 1)
+      @subject2 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 2, row_coordinate: 3)
   end
+
   subject do
     Piece.create(game_id: 1, user_id: 1, column_coordinate: 4, row_coordinate: 4)
   end
@@ -45,4 +47,42 @@ RSpec.describe Piece, type: :model do
       expect(subject.obstructed?(4, 7)).to eq false #checking above
     end
   end
+
+  describe 'method obstructed # checking if field are obstructed diagonal' do
+    it 'should return false if no piece exist in between' do
+      expect(subject.obstructed?(7,7)).to eq false
+      expect(subject.obstructed?(0,0)).to eq false
+    end
+    it 'should return true if there is a piece between destination and origin (upper right)' do
+      piece2 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 6, row_coordinate: 6)
+      piece3 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 5, row_coordinate: 6)
+      expect(subject.obstructed?(7,7)).to eq true
+      expect(@subject2.obstructed?(6,7)).to eq true
+    end
+    it 'should return true if there is a piece between destination and origin (lower left)' do
+      piece2 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 2, row_coordinate: 2)
+      piece3 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 1, row_coordinate: 2)
+      expect(subject.obstructed?(0,0)).to eq true
+      expect(@subject2.obstructed?(0,1)).to eq true
+    end
+    it 'should return true if there is a piece between destination and origin (upper left)' do
+      piece2 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 2, row_coordinate: 6)
+      expect(subject.obstructed?(1,7)).to eq true 
+    end
+    it 'should return true if there is a piece between destination and origin (lower right)' do
+      piece2 = Piece.create(game_id: 1, user_id: 1, column_coordinate: 6, row_coordinate: 2)
+      expect(subject.obstructed?(7,1)).to eq true
+    end
+  end
+
+    describe 'method obstructed # checking for invalid input' do
+      it "should return an error if destination is outside of board" do
+        expect(subject.obstructed?(8,8)).to eq 'Error Message'
+        expect(subject.obstructed?(-1,4)).to eq 'Error Message'
+        expect(subject.obstructed?(4,8)).to eq 'Error Message'
+      end
+      it "should return an error if move is invalid" do
+        expect(subject.obstructed?(1,5)).to eq 'Error Message'
+      end
+    end
 end
