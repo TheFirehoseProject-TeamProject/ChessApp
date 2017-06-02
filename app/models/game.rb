131c4b8b5251ba5e40b4615bdd3781488341424b
RaykPlatzek <rayk.platzek@gmail.com>
2017-06-02 00:01:53 +0000

    changes check on nil to check on NULL

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,5 +6,5 @@ class Game < ApplicationRecord
   belongs_to :black_player, class_name: 'User'
 
   enum game_status: { in_progress: 0, checkmate: 1, stalemate: 2 }
-  scope :available, -> { where('black_player_id = ? OR white_player_id = ?', nil, nil) }
+  scope :available, -> { where('black_player_id IS NULL OR white_player_id IS NULL') }
 end
