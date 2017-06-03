5b38580b5c2b675e0aecb37761ac16cf218839e7
Kristin Heininger <dauphindouze@comcast.net>
2017-06-03 15:38:14 +0000

    rewrote the rest of the test cases

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 RSpec.describe Game, type: :model do
   
   describe "#populate_board!" do
+
     it 'places the correct number of pawns on the board' do
-      
       white_player = FactoryGirl.create(:user)
       black_player = FactoryGirl.create(:user)
 
       game = FactoryGirl.create(:game, white_player: white_player, black_player: black_player )
-      # game = FactoryGirl.create(:game, params: { white_player: white_player, black_player: black_player })
-
+       # game = FactoryGirl.create(:game, params: { white_player: white_player, black_player: black_player })
       game.populate_board!
 
       expect(game.pieces.where(type: 'Pawn', color: 'white').count).to eq 8
+      expect(game.pieces.where(type: 'Pawn', color: 'black').count).to eq 8
       # expect(game.white_player.row_coordinate).to eq(1)
       # expect(game.white_player.column_coordinate).to eq(0..7)
       # expect(game.black_player.pawns.count).to eq 8
@@ -22,55 +22,59 @@ RSpec.describe Game, type: :model do
     end
 
     it 'places the correct number of rooks on the board' do
-      game = FactoryGirl.create(:game)
+      white_player = FactoryGirl.create(:user)
+      black_player = FactoryGirl.create(:user)
 
-      
+      game = FactoryGirl.create(:game, white_player: white_player, black_player: black_player )
       game.populate_board!
 
-      expect(game.rooks.white.count).to eq 2
-      
-      expect(game.rooks.black.count).to eq 2
+      expect(game.pieces.where(type: 'Rook', color: 'white').count).to eq 2
+      expect(game.pieces.where(type: 'Rook', color: 'black').count).to eq 2
       
     end
 
     it 'places the correct number of knights on the board' do
-      
-      game = FactoryGirl.create(:game)
+      white_player = FactoryGirl.create(:user)
+      black_player = FactoryGirl.create(:user)
 
+      game = FactoryGirl.create(:game, white_player: white_player, black_player: black_player )
       game.populate_board!
 
-      expect(game.knights.white.count).to eq 2
-      expect(game.knight.black.count).to eq 2
+      expect(game.pieces.where(type: 'Knight', color: 'white').count).to eq 2
+      expect(game.pieces.where(type: 'Knight', color: 'black').count).to eq 2
     end
 
-    it 'places the correct number of bishopss on the board' do
-      
-      game = FactoryGirl.create(:game)
+    it 'places the correct number of bishops on the board' do
+      white_player = FactoryGirl.create(:user)
+      black_player = FactoryGirl.create(:user)
 
+      game = FactoryGirl.create(:game, white_player: white_player, black_player: black_player )
       game.populate_board!
 
-      expect(game.bishops.white.count).to eq 2
-      expect(game.bishops.black.count).to eq 2
+      expect(game.pieces.where(type: 'Bishop', color: 'white').count).to eq 2
+      expect(game.pieces.where(type: 'Bishop', color: 'black').count).to eq 2
     end
 
     it 'places the correct number of kings on the board' do
-      
-      game = FactoryGirl.create(:game)
+      white_player = FactoryGirl.create(:user)
+      black_player = FactoryGirl.create(:user)
 
+      game = FactoryGirl.create(:game, white_player: white_player, black_player: black_player )
       game.populate_board!
 
-      expect(game.kings.white.count).to eq 1
-      expect(game.kings.black.count).to eq 1
+      expect(game.pieces.where(type: 'King', color: 'white').count).to eq 1
+      expect(game.pieces.where(type: 'King', color: 'black').count).to eq 1
     end
 
     it 'places the correct number of queens on the board' do
-      
-      game = FactoryGirl.create(:game)
+      white_player = FactoryGirl.create(:user)
+      black_player = FactoryGirl.create(:user)
 
+      game = FactoryGirl.create(:game, white_player: white_player, black_player: black_player )
       game.populate_board!
-
-      expect(game.queens.white.count).to eq 1
-      expect(game.queens.black.count).to eq 1
+      
+      expect(game.pieces.where(type: 'Queen', color: 'white').count).to eq 1
+      expect(game.pieces.where(type: 'Queen', color: 'black').count).to eq 1
     end
   end
     
