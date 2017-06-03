08bf62dff09edaa278d1dfe6daac3ab37f57c987
roderickjoseph <rjgauvin@gmail.com>
2017-06-03 19:16:07 +0000

    Removed commented out code

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -9,37 +9,6 @@ class Piece < ApplicationRecord
 
   def obstructed?(destination_x, destination_y)
     raise 'Error: Invalid Input' if destination_y > 7 || destination_x > 7 || destination_y < 0 || destination_x < 0
-    # invalid_move_tests =
-    #   [
-    #     horizontal_move?(destination_y),
-    #     vertical_move?(destination_x),
-    #     diagonal_up_and_right_move?(destination_x, destination_y),
-    #     diagonal_down_and_left_move?(destination_x, destination_y),
-    #     diagonal_up_and_left_move?(destination_x, destination_y),
-    #     diagonal_down_and_right_move?(destination_x, destination_y)
-    #   ]
-    # raise 'Error: Invalid Input' unless invalid_move_tests.include?(true)
-    # destination_tests = []
-    # invalid_move_tests.each_with_index do |test, index|
-    #   if test == true && index == 0
-    #     destination_tests << horizontal_obstruction_left?(destination_x)
-    #     destination_tests << horizontal_obstruction_right?(destination_x)
-    #   elsif test == true && index == 1
-    #     destination_tests << vertical_obstruction_up?(destination_y)
-    #     destination_tests << vertical_obstruction_down?(destination_y)
-    #   elsif test == true && index == 2
-    #     destination_tests << diagonal_obstruction_up_right?(destination_y)
-    #   elsif test == true && index == 3
-    #     destination_tests << diagonal_obstruction_down_left?(destination_y)
-    #   elsif test == true && index == 4
-    #     destination_tests << diagonal_obstruction_up_left?(destination_y)
-    #   elsif test == true && index == 5
-    #     destination_tests << diagonal_obstruction_down_right?(destination_y)
-    #   end
-    # end
-    # return true if destination_tests.include?(true)
-    # false
-      # byebug
     if horizontal_move?(destination_y)
       if horizontal_obstruction_left?(destination_x) || horizontal_obstruction_right?(destination_x)
         return true
