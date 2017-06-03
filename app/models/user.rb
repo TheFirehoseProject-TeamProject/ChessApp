378864aa03322a70337e2ab8ab67008d17da8868
roderickjoseph <rjgauvin@gmail.com>
2017-06-03 22:41:12 +0000

    Removed more commented out code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
   belongs_to :game
   has_many :pieces
 
