63e423ee17b6f863e6ecfda97408dc82bb9c140f
Kristin Heininger <dauphindouze@comcast.net>
2017-06-04 02:22:00 +0000

    Lists games on homepage

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,6 @@
 # Just a controller to set up an initial root page to test Heroku deployment
 class StaticPagesController < ApplicationController
-  def index; end
+  def index 
+    @games = Game.all
+  end
 end
