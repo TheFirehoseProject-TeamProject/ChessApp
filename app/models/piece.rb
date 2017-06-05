51f952db08838a52564c5aa0b1972532f8d6fe45
RaykPlatzek <rayk.platzek@gmail.com>
2017-06-05 00:01:36 +0000

    added schema to exclude list for rubocop and made all methods private again

--- app/models/piece.rb
+++ app/models/piece.rb
@@ -64,6 +64,8 @@ class Piece < ApplicationRecord
     raise 'Error: Invalid Input'
   end
 
+  private
+
   def horizontal_move?(destination_y)
     return true if destination_y == row_coordinate
     false
@@ -73,9 +75,6 @@ class Piece < ApplicationRecord
     return true if destination_x == column_coordinate
     false
   end
-
-  private
-
 
   def diagonal_up_and_right_move?(destination_x, destination_y)
     if destination_x > column_coordinate && destination_y > row_coordinate
