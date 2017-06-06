f2ff70f720e2c0582ff72bf8f2d62fb93837dd35
RaykPlatzek <rayk.platzek@gmail.com>
2017-06-06 04:14:19 +0000

    added new piece on the board and make it possible to move

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -12,21 +12,6 @@ class GamesController < ApplicationController
 
   private
 
-  def draw_board
-    board = Array.new(8) { Array.new(8) }
-    column = (1..8).to_a
-    row = (1..8).to_a
-    row.each_with_index do |_row, row_index|
-      column.each_with_index do |_column, column_index|
-        board[row_index][column_index] = if column_index.even? && row_index.even? || row_index.odd? && column_index.odd?
-                                           'white_field'
-                                         else
-                                           'black_field'
-                                         end
-      end
-    end
-    board
-  end
 
   def game_params
     params.require(:game).permit(:name, :number_of_moves, :black_player_id, :white_player_id, :game_status)
--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,2 +1,19 @@
 class PiecesController < ApplicationController
+  skip_before_filter :verify_authenticity_token, only: :update
+  def show
+    @piece = Piece.find(params[:id])
+    @board = draw_board
+  end
+
+  def update
+    @piece = Piece.find(params[:id])
+    @piece.update(piece_params)
+    redirect_to game_path(@piece.game_id)
+  end
+
+  private
+
+  def piece_params
+    params.require(:piece).permit(:row_coordinate, :column_coordinate, :type, :color)
+  end
 end
