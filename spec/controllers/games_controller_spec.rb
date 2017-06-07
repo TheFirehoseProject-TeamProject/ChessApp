6750296e90b2ddc7692b1b26d91782c93b1097c2
roderickjoseph <rjgauvin@gmail.com>
2017-06-07 23:07:47 +0000

    Adjusted games contoller show test to pass again.

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -4,11 +4,9 @@ require 'rails_helper'
 RSpec.describe GamesController, type: :controller do
   let(:game) { FactoryGirl.create(:game) }
 
-  # WIP - getting test to pass again
-
   describe 'games#show action' do
     it 'should show an empty chessboard' do
-      get :show, params: { id: current_game.id }
+      get :show, params: { id: game.id }
       expect(response).to have_http_status(:success)
     end
   end
