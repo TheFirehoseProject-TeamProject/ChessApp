a3e60ce9f0c08ad28fb356a86cd0b5585c7abc0f
RaykPlatzek <rayk.platzek@gmail.com>
2017-06-11 07:50:40 +0000

    1. changed board design so that A1 = row 0 and column 0 2. changed populating board so that is fits new board design 3. changed game and piece show page to fit new board design 4. added diagonal validations for pawn move 5. all rspec tests are green

--- app/models/pawn.rb
+++ app/models/pawn.rb
@@ -1,9 +1,14 @@
 class Pawn < Piece
   def valid_move?(destination_x, destination_y)
-    return true if vertical_move?(destination_x) && (destination_y - row_coordinate) == 1 && color == 'black'
-    return true if vertical_move?(destination_x) && (row_coordinate - destination_y) == 1 && color == 'white'
-    return true if vertical_move?(destination_x) && row_coordinate == 6 && (row_coordinate - destination_y) == 2 && color == 'white'
-    return true if vertical_move?(destination_x) && row_coordinate == 1 && (destination_y - row_coordinate) == 2 && color == 'black'
+    return true if vertical_move?(destination_x) && (destination_y - row_coordinate) == 1 && color == 'white'
+    return true if vertical_move?(destination_x) && (row_coordinate - destination_y) == 1 && color == 'black'
+    return true if vertical_move?(destination_x) && row_coordinate == 6 && (row_coordinate - destination_y) == 2 && color == 'black'
+    return true if vertical_move?(destination_x) && row_coordinate == 1 && (destination_y - row_coordinate) == 2 && color == 'white'
+    if color == 'white' && !Piece.where(column_coordinate: destination_x, row_coordinate: destination_y).empty?
+      return true if diagonal_up_and_right_move?(destination_x, destination_y) || diagonal_up_and_left_move?(destination_x, destination_y)
+    elsif color == 'black' && !Piece.where(column_coordinate: destination_x, row_coordinate: destination_y).empty?
+      return true if diagonal_down_and_right_move?(destination_x, destination_y) || diagonal_down_and_left_move?(destination_x, destination_y)
+    end
     false
   end
 end
