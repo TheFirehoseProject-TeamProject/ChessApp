f81fd860115aa98203fb786d88f4bf7624f86220
RaykPlatzek <rayk.platzek@gmail.com>
2017-06-12 02:15:16 +0000

    routes fixed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
-    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session_facebook
   end
 
   root 'static_pages#index'
