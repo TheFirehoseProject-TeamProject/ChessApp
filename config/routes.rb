78ad6965c5d43386dd6b46275a817bbbf0821dce
RaykPlatzek <rayk.platzek@gmail.com>
2017-06-12 02:04:27 +0000

    added new_with_session method to User model and logout links to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
+
   root 'static_pages#index'
   resources :games, only: %i[new create show]
   resources :pieces, only: %i[show update]
