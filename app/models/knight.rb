756c461383047331521ad6886ed8f50916cdc451
roderickjoseph <rjgauvin@gmail.com>
2017-06-14 04:44:34 +0000

    Code cleanup

--- app/models/knight.rb
+++ app/models/knight.rb
@@ -1,16 +1,5 @@
 class Knight < Piece
   def valid_move?(destination_x, destination_y)
-    # %i[
-    #   up_one_right_two
-    #   up_two_right_one
-    #   weird_test?
-    #
-    # ].each do |check|
-    #   return true if self.send(check, destination_x, destination_y)
-    # end
-    #
-    # false
-
     return true if up_one_right_two?(destination_x, destination_y)
 
     return true if up_two_right_one?(destination_x, destination_y)
@@ -31,17 +20,6 @@ class Knight < Piece
   end
 
   private
-
-  # class Move
-  #   def initialize(from_x, from_y, to_x, to_y)
-  #
-  #   def valid?
-  #   end
-  #
-  #   def up_one_right_two?
-  #     to_x == from_x + 2 .....
-  # end
-
 
   def up_one_right_two?(destination_x, destination_y)
     destination_x == column_coordinate + 2 && destination_y == row_coordinate + 1
