d57e83ac6fb8636c1fce543c41e417b66ffcd829
Daniel <danielsfskim@gmail.com>
2017-06-16 19:06:26 +0000

    rook add-ons

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -6,10 +6,10 @@ RSpec.describe Rook, type: :model do
   let(:rook) { FactoryGirl.create(:rook, :is_on_board) }
 
   describe 'is rook.valid_move?' do
-    expect(rook.valid_move?(4,6)). to eq true
-    expect(rook.valid_move?(6,5)). to eq false
-    expect(rook.valid_move?(7,7)). to eq false
-    expect(rook.valid_move?(6,2)). to eq false
-    expect(rook.valid_move?(8,4)). to eq true
-    end
+    expect(rook.valid_move?(4, 6)). to eq true
+    expect(rook.valid_move?(6, 5)). to eq false
+    expect(rook.valid_move?(7, 7)). to eq false
+    expect(rook.valid_move?(6, 2)). to eq false
+    expect(rook.valid_move?(8, 4)). to eq true
   end
+end
