4c3db648b8cc4c5b3d4ab71dd497f9463570a497
Daniel <danielsfskim@gmail.com>
2017-06-17 21:57:31 +0000

    block given

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,6 +1,7 @@
 class Rook < Piece
   def valid_move?(destination_x, destination_y)
-    return false if obstructed?(destination_x, destination_y)
+    if obstructed?(destination_x, destination_y)
+      return false
     if vertical_move?(destination_x) || horizontal_move?(destination_y)
       return true
     end
