a681ab871ffc2dc1040eb41c4c771294b317ebd3
Daniel <danielsfskim@gmail.com>
2017-06-17 20:46:49 +0000

    obstructed error

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -1,19 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Rook, type: :model do
-  let(:game) { FactoryGirl.create(:game) }
-  let(:user) { FactoryGirl.create(:user) }
-  let(:rook) { FactoryGirl.create(:rook, :is_on_board) }
+  describe "is valid move? will return false if it is not allowed" do
+  before :each do
+    game = FactoryGirl.create(:join_game)
+    rook = Rook.create(destination_x: 4, destination_y: 4)
+  end
 
-  describe 'rook moves' do
-    it 'should be a valid move' do
-      expect(rook.valid_move?(4, 5)). to eq true
-      expect(rook.valid_move?(0, 4)). to eq true
+    it "returns false if moving 2 spaces up" do
+      expect(rook.valid_move?(4,6)).to eq true
     end
-
-    it 'should be an invalid move' do
-      expect(rook.valid_move?(4, 5)). to raise_error('Error: Invalid Input')
-      
+    it "returns false if moves like a knight" do
+      expect(rook.valid_move?(6,5)).to eq false
+    end
+    it "returns true if moves diagonal 3 spaces" do
+      expect(rook.valid_move?(7,7)).to eq false
+    end 
+    it "returns true if moves diagonal 2 spaces" do
+      expect(rook.valid_move?(6,2)).to eq false
+    end 
+    it "returns false if it tries to move horizontally" do
+      expect(rook.valid_move?(8,4)).to eq true
     end
   end
 end
