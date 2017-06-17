7a93a464642c1d93fef990af2a458089ec9502d2
Daniel <danielsfskim@gmail.com>
2017-06-17 18:07:46 +0000

    rook add-ons

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,7 +1,7 @@
 class Rook < Piece
   def valid_move?(destination_x, destination_y)
     return Error: Invalid Input if obstructed?(destination_x, destination_y)
-    if vertical_move?(destination_x, destination_y) || horizontal_move?(destination_x, destination_y)
+    if vertical_move?(destination_x) || horizontal_move?(destination_y)
       return true
     end
     false
