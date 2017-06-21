5ad4d6ff0b1d6c38e0a3975e64bd39170a32f830
Daniel <danielsfskim@gmail.com>
2017-06-21 18:03:55 +0000

    error fixes

--- app/models/rook.rb
+++ app/models/rook.rb
@@ -1,7 +1,6 @@
 class Rook < Piece
   def valid_move?(destination_x, destination_y)
-    if obstructed?(destination_x, destination_y)
-      return false
+    return false if obstructed?(destination_x, destination_y)
     if vertical_move?(destination_x) || horizontal_move?(destination_y)
       return true
     end
