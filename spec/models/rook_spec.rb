9c13d8705f8e8d7df7dfbb58491f4dcc6603dfd5
Daniel S. Kim <danielsfskim@gmail.com>
2017-06-24 19:05:37 -0700

    update rook_spec.rb

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -11,7 +11,7 @@ RSpec.describe Rook, type: :model do
       expect(rook.valid_move?(0, 4)).to eq true
     end
 
-    it 'should raise_error' do
+    it 'should not be a valid move' do
       expect(rook.valid_move?(3, 3)).to eq false
       expect(rook.valid_move?(5, 5)).to eq false
     end
