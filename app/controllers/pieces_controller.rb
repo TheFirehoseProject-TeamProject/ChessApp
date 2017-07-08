2270dee14420146b63fce2e3ce0d8591ea1c7996
RP Engineering <rayk.platzek@gmail.com>
2017-07-08 21:10:42 +1000

    undo refactoring
    refactoring caused some issues therefore I decided to undo it for now.

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,20 +1,8 @@
 class PiecesController < ApplicationController
   skip_before_action :verify_authenticity_token, only: :update
-
   def update
     @piece = Piece.find(params[:id])
-    destination_x = piece_params[:column_coordinate].to_i
-    destination_y = piece_params[:row_coordinate].to_i
-    flashmessages
-    render plain: 'Invalid Move', status: :bad_request unless checks_before_move(destination_x, destination_y) == true
-    current_row = @piece.row_coordinate
-    current_column = @piece.column_coordinate
-    en_passant_flag = @piece.game.piece_capturable_by_en_passant
-    destination_piece = @piece.find_destination_piece(destination_x, destination_y).present?
-    @piece.move_to!(destination_x, destination_y)
-    change_turn
-    current_game.set_game_status
-    trigger_pusher(current_row, current_column, en_passant_flag, destination_piece)
+    checks_before_move
   end
 
   private
@@ -27,22 +15,32 @@ class PiecesController < ApplicationController
     params.require(:piece).permit(:row_coordinate, :column_coordinate, :type, :color, :id)
   end
 
-  def trigger_pusher(current_row, current_column, en_passant_flag, destination_piece)
-    Pusher.trigger('piece_moved_game' + @piece.game.id.to_s, 'piece_moved', message: flashmessages,
-                                                                            turn: check_turn,
-                                                                            type: @piece.type,
-                                                                            color: @piece.color,
-                                                                            destination_piece: destination_piece,
-                                                                            en_passant_id: en_passant_flag,
-                                                                            row_destination: piece_params[:row_coordinate].to_i,
-                                                                            column_destination: piece_params[:column_coordinate].to_i,
-                                                                            row_origin: current_row,
-                                                                            column_origin: current_column)
-  end
-
-  def checks_before_move(destination_x, destination_y)
+  def checks_before_move
+    flashmessages
+    destination_x = piece_params[:column_coordinate].to_i
+    destination_y = piece_params[:row_coordinate].to_i
+    current_row = @piece.row_coordinate
+    current_column = @piece.column_coordinate
+    en_passant_flag = @piece.game.piece_capturable_by_en_passant
+    destination_piece = @piece.find_destination_piece(destination_x, destination_y).present?
     render plain: 'Invalid Move', status: :bad_request if @piece.not_moved?(destination_x, destination_y)
-    return true if !@piece.obstructed?(destination_x, destination_y) && @piece.valid_move?(destination_x, destination_y) && check_turn == @piece.color
+    if !@piece.obstructed?(destination_x, destination_y) && @piece.valid_move?(destination_x, destination_y) && check_turn == @piece.color
+      @piece.move_to!(destination_x, destination_y)
+      change_turn
+      current_game.set_game_status
+      Pusher.trigger('piece_moved_game' + @piece.game.id.to_s, 'piece_moved', message: flashmessages,
+                                                                              turn: check_turn,
+                                                                              type: @piece.type,
+                                                                              color: @piece.color,
+                                                                              destination_piece: destination_piece,
+                                                                              en_passant_id: en_passant_flag,
+                                                                              row_destination: destination_y,
+                                                                              column_destination: destination_x,
+                                                                              row_origin: current_row,
+                                                                              column_origin: current_column)
+    else
+      render plain: 'Invalid Move', status: :bad_request
+    end
   end
 
   def flashmessages
