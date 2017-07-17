2dc6da6ff2514929fa90539f13db7813b6e1b58c
Kristin Heininger <dauphindouze@comcast.net>
2017-07-17 21:32:10 +0000

    Fixed rook and queen being able to move to their current position.

--- spec/models/rook_spec.rb
+++ spec/models/rook_spec.rb
@@ -15,5 +15,9 @@ RSpec.describe Rook, type: :model do
       expect(rook.valid_move?(3, 3)).to eq false
       expect(rook.valid_move?(5, 5)).to eq false
     end
+
+    it 'should not be a valid move' do
+      expect(rook.valid_move?(4, 4)).to eq(false)
+    end
   end
 end
